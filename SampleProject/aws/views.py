b7897108d9320b275ed54ef1c6629b61446406f2
Prateek Gupta <prateek.gupta25apr@gmail.com>
2024-07-03 22:52:56 +0530

    Added code to get creds rom AWS CLI

--- SampleProject/aws/views.py
+++ SampleProject/aws/views.py
@@ -2,17 +2,27 @@ import boto3
 from django.conf import settings
 from django.http import HttpResponse
 
+from main.LogManager import logger
 from util import get_success_response
 from util.exceptions import log_error, SampleProjectException
 
 
 def get_s3_client():
-    return boto3.client(
+    # Fetching from AWS CLI
+    s3_client = boto3.client('s3')
+
+    if s3_client is None:
+        # Preparing client from creds
+        s3_client = boto3.client(
             's3',
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             region_name=settings.AWS_S3_REGION_NAME,
         )
+    else:
+        logger.error("Got creds from AWS CLI")
+
+    return s3_client
 
 
 def upload_file(request):
