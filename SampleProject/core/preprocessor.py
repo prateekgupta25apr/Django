ef68a42b9cecff5259b55fc6c56dcd2d8c8b670b
Prateek Gupta <prateek@wolkensoftware.com>
2025-12-17 14:55:09 +0530

    Updated preprocessor

--- SampleProject/core/preprocessor.py
+++ SampleProject/core/preprocessor.py
@@ -3,4 +3,7 @@
 def preprocessed_data(request):
     """This method will provide few commonly required data to the templates, which can be
     accessed by using double curly braces(i.e. {{}})"""
-    return {"pre_processor": "pre_processor_data"}
+    return {
+        "static": "pre_processor_data",
+        "dynamic": request.method
+    }
