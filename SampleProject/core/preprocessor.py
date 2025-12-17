9f2ba9eddf565861cf18e8b863f6105836a15a13
Prateek Gupta <prateek@wolkensoftware.com>
2025-12-17 13:36:56 +0530

    Added preprocessor.py

---
+++ SampleProject/core/preprocessor.py
@@ -0,0 +1,6 @@
+
+
+def preprocessed_data(request):
+    """This method will provide few commonly required data to the templates, which can be
+    accessed by using double curly braces(i.e. {{}})"""
+    return {"pre_processor": "pre_processor_data"}
