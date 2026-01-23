7afa4e864f6dc54b3287649eefb2e48f11b6486e
Prateek Gupta <prateek.gupta25apr@gmail.com>
2026-01-24 00:36:07 +0530

    Updated Email urls

--- SampleProject/emails/emails_urls.py
+++ SampleProject/emails/emails_urls.py
@@ -6,7 +6,7 @@ from . import emails_async_views as av
 urlpatterns = [
     path('async/get_email_content', av.get_email_content),
     path('async/send_email', av.send_email),
-    path('async/process_email', sv.process_email_request),
+    path('async/process_email', av.process_email_request),
 
     path('sync/get_email_content', sv.get_email_content),
     path('sync/send_email', sv.send_email),
